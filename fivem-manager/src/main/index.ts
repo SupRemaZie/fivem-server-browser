18b8cdc60e33f079e427d024e4a2da01df53996c
DamDev <dporchet1110@gmail.com>
2025-11-26 15:25:29 +0100

    test

--- fivem-manager/src/main/index.ts
+++ fivem-manager/src/main/index.ts
@@ -1,7 +1,6 @@
 import { app, shell, BrowserWindow } from 'electron'
 import { join } from 'path'
 import { electronApp, optimizer, is } from '@electron-toolkit/utils'
-import icon from '../../resources/icon.png?asset'
 import { initDatabase, getDatabase, closeDatabase } from './db'
 import { registerServerHandlers } from './handlers/servers'
 import { registerPlayerHandlers } from './handlers/players'
@@ -17,7 +16,6 @@ function createWindow(): void {
     minHeight: 600,
     show: false,
     autoHideMenuBar: true,
-    ...(process.platform === 'linux' ? { icon } : {}),
     webPreferences: {
       preload: join(__dirname, '../preload/index.js'),
       sandbox: false
